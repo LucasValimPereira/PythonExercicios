742b91a8925ed7694689647cfe69b5b40a001874
LucasValimPereira <lucasvalimp5@rede.ulbra.br>
2026-09-25 17:53:45 -0300

    exercicio 24 concluido

---
+++ ex024.py
@@ -0,0 +1,5 @@
+cidade = str(input('Digite o nome da cidade para ver se existe a palavra "Santo" nela: '))
+
+verdadeiro = "Santo" in cidade
+
+print("A sua cidade possui a palavra 'Santo'?{}".format(verdadeiro))
