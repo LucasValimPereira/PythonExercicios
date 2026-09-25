9ab30d6e4128f0f8f70629f8731a168c96137a31
LucasValimPereira <lucasvalimp5@rede.ulbra.br>
2026-09-25 17:31:48 -0300

    exercicio 23 concluido

---
+++ ex023.py
@@ -0,0 +1,13 @@
+digito = str(input("Digite um número: "))
+
+milhar = digito[0]
+centena = digito[1]
+dezena = digito[2]
+unidade = digito[3]
+
+print(f"Unidade: {unidade}")
+print(f"Dezena: {dezena}") 
+print(f"Centena: {centena}")
+print(f"Milhar: {milhar}")
+
+
